a5a158062d53a8574088f62d203c44c91c9e1804
Rajbir Kataria <raj@vpnpool-130-126-152-97.near.illinois.edu>
2016-01-13 10:42:19 -0600

    Added scheduler template

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ var app = express();
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
+app.use('/js3',serveStatic(__dirname + '/scripts/third_party'));
+
 app.get('/', function (req, res) {
   var person = 'Raj';
   res.render('main.ejs', {
     name: person
   });
 });
+
+app.get('/schedule', function (req, res) {
+  res.render('schedule.ejs');
+});
+
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!');
