ef7d661bf3e662fd2de5214c189e99d5d2b74ce6
Rajbir Kataria <raj@Rajbirs-MacBook-Pro.local>
2016-01-16 11:39:07 -0600

    Adding potree project with an example

--- server.js
+++ server.js
@@ -5,19 +5,23 @@ var app = express();
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 app.use('/js3',serveStatic(__dirname + '/scripts/third_party'));
+app.use('/resources',serveStatic(__dirname + '/scripts/third_party/potree/resources'));
 
 app.get('/', function (req, res) {
-  var person = 'Raj';
-  res.render('main.ejs', {
-    name: person
-  });
+	var person = 'Raj';
+	res.render('main.ejs', {
+		name: person
+	});
 });
 
 app.get('/schedule', function (req, res) {
-  res.render('schedule.ejs');
+	res.render('schedule.ejs');
 });
 
+app.get('/viewer', function (req, res) {
+	res.render('viewer.ejs');
+});
 
 app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+	console.log('App listening on port 3000!');
 })
